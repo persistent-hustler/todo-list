fe12aa6ad3e21e9c9bf86c70a2c8e3ca13aed6e8
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-09-11 19:20:14 +0530

    Implement Add Note

--- src/index.js
+++ src/index.js
@@ -6,11 +6,6 @@ import Todo from "./modules/todo-class";
 import showCard from "./dom/show-card";
 
 
-new Todo(projectsList.default, "Title", "Description", new Date(), 1);
-new Todo(projectsList.project1, "Title1", "Description", new Date(), 1);
-new Todo(projectsList.project2, "Title2", "Description", new Date(), 1);
-
-
 // let displayCards = todosArray;
 
 // console.log(projectsList);
@@ -21,6 +16,72 @@ new Todo(projectsList.project2, "Title2", "Description", new Date(), 1);
 
 // console.log(displayCards);
 projectsDOM(projectsList);
-showCard(todosArray[0]);
-showCard(todosArray[1]);
-showCard(todosArray[2]);
+
+const addNoteDialog = document.createElement('dialog');
+
+addNoteDialog.innerHTML = `
+<form method="dialog">
+    <div>
+        <label for='new-title'> Title:
+            <input id='new-title' type='text' required>
+        </label>
+    </div>
+    <div>
+        <label for="new-project"> Project:
+            <select name="project" id="new-project">
+                <option value="${projectsList.default}">Default</option>
+                <option value="${projectsList.project1}">Project1</option>
+                <option value="${projectsList.project2}">Project2</option>
+                <option value="${projectsList.project3}">Project3</option>
+            </select>
+        </label>
+    </div>
+    <div>
+        <label for='new-description'> Description:
+            <textarea id='new-description'></textarea>
+        </label>
+    </div>
+    <div>
+        <label for='new-due-date'> DueDate:
+            <input id='new-due-date' type='date'>
+        </label>
+    </div>
+    <div>
+        <label for='new-priority'> Priority:
+            <input id='new-priority' type='text'>
+        </label>
+    </div>
+        
+    <div>
+        <button id='close-dialog' type='cancel'>Close</button>
+        <button id='save-new-note' type='submit'>Save</button>
+    </div>
+</form>`;
+document.getElementById('display-container').appendChild(addNoteDialog);
+
+const addNoteBtn = document.querySelector('#new-note');
+
+addNoteBtn.addEventListener('click', ()=>{
+    addNoteDialog.showModal();
+});
+
+const tasksHeading = document.querySelector('#tasks-heading');
+const cardsContainer = document.querySelector('#cards-container');
+
+const saveNote = document.getElementById('save-new-note');
+saveNote.addEventListener('click', ()=>{
+    const project = document.getElementById('new-project').value;
+    const title = document.getElementById('new-title').value;
+    const description = document.getElementById('new-description').value;
+    let dueDate = document.getElementById('new-due-date').value;
+    if(!dueDate) {
+        dueDate = new Date();
+    }
+    const priority = document.getElementById('new-priority').value;
+    const todo = new Todo(project, title, description, dueDate, priority);
+    if(title) {
+        tasksHeading.innerText = "All Tasks";
+        cardsContainer.innerHTML = '';
+        todosArray.forEach(todo=>{showCard(todo)});
+    }
+});
