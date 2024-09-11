737722aadc22eca761af4a59ffbab0db2007e4a2
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-09-11 18:10:16 +0530

    Display cards only of selected project

--- src/dom/projects-bar.js
+++ src/dom/projects-bar.js
@@ -1,11 +1,17 @@
+import todosArray from "../data/todos-array";
+import showCard from "./show-card";
+
 export function projectsDOM(projectsList) {
     const projectsContainer = document.querySelector('#projects-container');
     const tasksHeading = document.querySelector('#tasks-heading');
+    const cardsContainer = document.querySelector('#cards-container');
 
     const allButton = document.createElement('button');
     allButton.innerText = 'All';
     allButton.addEventListener('click', ()=>{
         tasksHeading.innerText = "All Tasks";
+        cardsContainer.innerHTML = '';
+        todosArray.forEach(todo=>{showCard(todo)});
     });
     projectsContainer.appendChild(allButton);
 
@@ -14,6 +20,8 @@ export function projectsDOM(projectsList) {
         project.innerText = projectsList[key];
         project.addEventListener('click', ()=>{
             tasksHeading.innerText = projectsList[key];
+            cardsContainer.innerHTML = '';
+            todosArray.filter(todo=>todo.project===projectsList[key]).forEach(todo=>{showCard(todo)});
         });
         projectsContainer.appendChild(project);
     });
